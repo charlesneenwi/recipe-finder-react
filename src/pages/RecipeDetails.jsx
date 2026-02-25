8c2d128b6d771a21480722c4f44bb73a868c20a5
charles neenwi <charlesneenwi@gmail.com>
2026-02-25 12:58:48 +0100

    feat: add back navigation button to recipe details page

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const RecipeDetails = () => {
+  const navigate = useNavigate();
 
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
@@ -33,6 +35,14 @@ const RecipeDetails = () => {
   return (
     <div className="max-w-5xl mx-auto p-6">
       <NavBar />
+
+      <button
+  onClick={() => navigate(-1)}
+  className="mb-6 px-4 py-2 bg-gray-200 rounded hover:bg-gray-300 transition"
+>
+  ← Back
+      </button>
+      
     {/* Image */}
     <img
       src={recipe.strMealThumb}
